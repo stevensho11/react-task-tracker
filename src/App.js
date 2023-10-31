a9f678125aaaf37e6f4092e8afab4874718f4826
Stevens Ho <stevensho@stevensho.com>
2023-10-30 20:06:31 -0400

    Practicing passing state down and actions up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import Header from "./Components/Header";
+import Tasks from "./Components/Tasks";
+import { useState } from "react";
 
 function App() {
-  const name = "Stevens";
-  const x = false;
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Wake Up",
+      day: "October 31 at 9:00AM",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Ice Bucket Challenge",
+      day: "October 31 at 9:05AM",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Back to Sleep",
+      day: "October 31 at 9:10AM",
+      reminder: false,
+    },
+  ]);
+
+  //Delete a task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
 
   return (
     <div className="container">
-      <Header title={1} />
+      <Header title="Task Tracker" />
+      <Tasks tasks={tasks} onDelete={deleteTask} />
     </div>
   );
 }
