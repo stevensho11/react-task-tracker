1832c779fe50ab4ab11f14a3405e4bff7bd335e6
Stevens Ho <stevensho@stevensho.com>
2023-10-30 21:16:06 -0400

    Testing addition of db.json to simulate REST api

--- src/App.js
+++ src/App.js
@@ -2,29 +2,11 @@ import Header from "./Components/Header";
 import Tasks from "./Components/Tasks";
 import { useState } from "react";
 import AddTask from "./Components/AddTask";
+import { useEffect } from "react";
 
 function App() {
   const [showAddTask, setShowAddTask] = useState(false);
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: "Wake Up",
-      day: "October 31 at 9:00AM",
-      reminder: true,
-    },
-    {
-      id: 2,
-      text: "Ice Bucket Challenge",
-      day: "October 31 at 9:05AM",
-      reminder: true,
-    },
-    {
-      id: 3,
-      text: "Back to Sleep",
-      day: "October 31 at 9:10AM",
-      reminder: false,
-    },
-  ]);
+  const [tasks, setTasks] = useState([]);
 
   //Add a task
   const addTask = (task) => {
@@ -33,6 +15,22 @@ function App() {
     setTasks([...tasks, newTask]);
 
     console.log(id);
+  };
+
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks();
+      setTasks(tasksFromServer);
+    };
+    getTasks();
+  }, []);
+
+  const fetchTasks = async () => {
+    const res = await fetch("http://localhost:5000/tasks");
+    const data = await res.json();
+
+    console.log(data);
+    return data;
   };
 
   //Delete a task
